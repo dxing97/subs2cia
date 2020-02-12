e88c9af9686325a7c792e6eb4a9d400fe2071526
dxing97 <danielxing97@gmail.com>
2020-02-11 20:21:05 -0500

    update README.md   added basic installation instructions   added some usage examples fix videoless input resulting in nothing happening implemented verbose mode, and made it a little more verbose updated TODO.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="subs2cia",
-    version="0.1.2",
+    version="0.1.3",
     license='MIT',
     author="Daniel Xing",
     author_email="danielxing97@gmail.com",

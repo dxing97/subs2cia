9650c1ed15212ffe7568b09606d0387cce00890a
Daniel Xing <danielxing97@gmail.com>
2021-04-17 09:51:47 -0400

    update setup.py to allow python 3.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     install_requires=[
         "ffmpeg-python",
         "pycountry",
